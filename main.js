5d07db79c8fbd52d8cf3d800a364c0de0fb839ba
kaji <kaji.masahiro0930@docomo.ne.jp>
2023-07-11 17:11:05 +0900

    kaji

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import player from "~/.player"
 import enemy from "~./enemy"
 import map from "./map";
 
-const SCREEN_WIDTH  = 1920;
-const SCREEN_HEIGHT = 1080;
+const SCREEN_WIDTH  = 1024;
+const SCREEN_HEIGHT = 576;
 
 const canvas = document.getElementById("can");
 const ctx = canvas.getContext("2d");
