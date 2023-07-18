f706408650f63c0699a01c6ac92e87c9781fef11
O-Ryousuke <pzze35r6@s.okayama-u.ac.jp>
2023-07-18 15:54:14 +0900

    ogawa

--- bullet.js
+++ bullet.js
@@ -1,5 +1,28 @@
-class bullet{
-
+//弾クラス
+class bullet
+{
+	constructor( x,y, vx,vy )
+	{
+		this.sn   = 6;//スプライト配列番号
+		this.x    = x;
+		this.y    = y;
+		this.vx   = vx;
+		this.vy   = vy;
+		this.kill = false;//tama配列が増えすぎないようにフラグを入れる
+	}
+	
+	update()
+	{
+		this.x += this.vx;
+		this.y += this.vy;
+		
+		if( this.x<0 || this.x>FIELD_W<<8
+				|| this.y<0 || this.y>FIELD_H<<8 )this.kill = true;
+		//画面外でkillフラグをon
+	}
+	
+	draw()
+	{
+		drawSprite( this.sn, this.x, this.y );
+	}
 }
-
-export default bullet;
