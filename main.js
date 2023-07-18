1232297fb36e8209931c8494aedf176e3fd84ad2
kaji <kaji.masahiro0930@docomo.ne.jp>
2023-07-18 15:55:52 +0900

    kaji

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-import player from "~/.player";
-import enemy from "~./enemy";
-import map from "./map";
-
 const SCREEN_WIDTH  = 1024;
 const SCREEN_HEIGHT = 576;
 const GAME_SPEED = 1000/60;
+const scene = 1;
 
 const canvas = document.getElementById("can");
 const ctx = canvas.getContext("2d");
@@ -12,12 +9,22 @@ const ctx = canvas.getContext("2d");
 //ゲーム初期化
 function gameInit()
 {
+    scene = 1;
 	setInterval( gameLoop , GAME_SPEED );
 }
 //ゲームループ
 function gameLoop()
 {
-    //描画
+    
+    if(scene==1) //タイトル
+    {
+
+    }
+    else if(scene==2) //ゲームの部分
+    {
+
+    }
+
 }
 
 window.onload=function()
--- map.js
+++ map.js
@@ -1,5 +1,4 @@
 class map{
 
 }
-
 export default map;
