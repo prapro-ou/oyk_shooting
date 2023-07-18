85c7e1b7eee77c34bdf146d837fc7a144fb83ddb
O-Ryousuke <pzze35r6@s.okayama-u.ac.jp>
2023-07-18 18:21:46 +0900

    attached atk

--- bullet.js
+++ bullet.js
@@ -1,13 +1,13 @@
 //弾クラス
 class bullet
 {
-	constructor( x,y, vx,vy )
+	constructor( x,y, vx,vy,atk )
 	{
-		this.sn   = 6;//スプライト配列番号
 		this.x    = x;
 		this.y    = y;
 		this.vx   = vx;
 		this.vy   = vy;
+		this.atk  = atk;//攻撃力
 		this.kill = false;//tama配列が増えすぎないようにフラグを入れる
 	}
 	
@@ -19,6 +19,14 @@ class bullet
 		if( this.x<0 || this.x>FIELD_W<<8
 				|| this.y<0 || this.y>FIELD_H<<8 )this.kill = true;
 		//画面外でkillフラグをon
+		
+		
+		if( hit()=true) kill = true;
+		//敵の弾とプレイヤーまたはプレーヤーの弾と敵の衝突によりkillフラグをon
+		
+		//壁との衝突は弾を消す？貫通？反射？
+		
+		//
 	}
 	
 	draw()
